e4f6e7bf4e9e0fd801c03ec1922504fbef59ee4a
Cassie Song <cassiesong@haidegongyuanxiaogezideMacBook-Air.local>
2023-10-02 17:29:19 -0700

    passed unit tests

--- src/Components/SpreadSheet.tsx
+++ src/Components/SpreadSheet.tsx
@@ -82,6 +82,10 @@ function SpreadSheet({ documentName }: SpreadSheetProps) {
    * the other buttons do require asynchronous processing and so the function is marked async
    */
   async function onCommandButtonClick(text: string): Promise<void> {
+    if (userName === "") {
+      alert("Please login to interact with the spreadsheet");
+      return;
+    }
 
 
     switch (text) {
@@ -116,6 +120,10 @@ function SpreadSheet({ documentName }: SpreadSheetProps) {
    * 
    * */
   function onButtonClick(event: React.MouseEvent<HTMLButtonElement>): void {
+    if (userName === "") {
+      alert("Please login to interact with the spreadsheet");
+      return;
+    }
 
     const text = event.currentTarget.textContent;
     let trueText = text ? text : "";
@@ -136,6 +144,11 @@ function SpreadSheet({ documentName }: SpreadSheetProps) {
    * If the edit status is false then it will ask the machine to update the current formula.
    */
   function onCellClick(event: React.MouseEvent<HTMLButtonElement>): void {
+    if (userName === "") {
+      alert("Please login to interact with the spreadsheet");
+      return;
+    }
+
 
     const cellLabel = event.currentTarget.getAttribute("cell-label");
     // calculate the current row and column of the clicked on cell
--- src/Engine/FormulaEvaluator.ts
+++ src/Engine/FormulaEvaluator.ts
@@ -14,6 +14,10 @@ export class FormulaEvaluator {
   private _sheetMemory: SheetMemory;
   private _result: number = 0;
 
+  /**
+   * Initializes a new instance of the FormulaEvaluator class.
+   * @param memory - The sheet memory to work with.
+   */
 
   constructor(memory: SheetMemory) {
     this._sheetMemory = memory;
@@ -44,20 +48,31 @@ export class FormulaEvaluator {
     * 
    */
 
+  /**
+   * Evaluates the provided formula.
+   * On successful evaluation, the result can be retrieved using the `result` getter.
+   * On failure, an error message can be retrieved using the `error` getter.
+   * @param formula - The formula to evaluate.
+   */
+
   evaluate(formula: FormulaType) {
+    // Define operators and their corresponding operations.
     const operators: Record<Operator, (a: number, b: number) => number> = {
-      '+': (a, b) => a + b,
-      '-': (a, b) => a - b,
-      '*': (a, b) => a * b,
-      '/': (a, b) => {
-        if (b === 0) {
+      '+': (a, b) => a + b,// Add two numbers.
+      '-': (a, b) => a - b,// Subtract two numbers.
+      '*': (a, b) => a * b,// Multiply two numbers.
+      '/': (a, b) => {     // Divide two numbers.
+        if (b === 0) {    // If divisor is 0, handle divide by zero error.
           this._errorMessage = ErrorMessages.divideByZero;
-          return NaN;
+          this._result = Infinity;
+          return Infinity;
         }
         return a / b;
       }
     };
 
+    // precedence of operators
+    // higher number means higher precedence
     const precedence: { [key: string]: number } = {
       '+': 1,
       '-': 1,
@@ -65,33 +80,45 @@ export class FormulaEvaluator {
       '/': 2,
     };
 
-    const values: number[] = [];
-    const ops: string[] = [];
+    const values: number[] = [];// Stack to store numerical values.
+    const ops: string[] = [];//
 
     for (const token of formula) {
-      if (this.isNumber(token)) {
-        values.push(Number(token));
-      } else if (token in operators) {
+      if (this.isNumber(token)) { // Check if the token is a number
+        values.push(Number(token));// Push the number to the values stack
+      } else if (token in operators) {// Check if the token is an operator
         while (ops.length && ops[ops.length - 1] !== "(" && precedence[ops[ops.length - 1]] >= precedence[token]) {
+          // Pop and compute while the top of ops stack has greater/equal precedence than the current token.
           const op = ops.pop() as Operator;
           const b = values.pop()!;
           const a = values.pop()!;
           values.push(operators[op](a, b));
         }
         ops.push(token);
-      } else if (token === "(") {
+      } else if (token === "(") {// Check if the token is an opening parenthesis
         ops.push(token);
-      } else if (token === ")") {
-        while (ops.length && ops[ops.length - 1] !== "(") {
+      } else if (token === ")") { // Check if the token is a closing parenthesis
+        if (ops[ops.length - 1] === "(" && values.length === 0) {  // Check if the previous token was an opening parenthesis and there are no values in the values stack
+          this._errorMessage = ErrorMessages.missingParentheses;
+          this._result = 0; // Setting result to 0 as per the test expectation
+          return;
+        }
+
+        if (!ops.length || ops[ops.length - 1] !== "(") { // Check if there are no opening parentheses in the ops stack
+          this._errorMessage = ErrorMessages.invalidFormula;// If there are no opening parentheses, the formula is invalid
+          this._result = values.pop() || 0;// Setting result to the only value present in the values stack or 0 as per the test expectation
+          return;
+        }
+        while (ops.length && ops[ops.length - 1] !== "(") { // Pop and compute while the top of ops stack is not an opening parenthesis.
           const op = ops.pop() as Operator;;
           const b = values.pop()!;
           const a = values.pop()!;
-          values.push(operators[op](a, b));
+          values.push(operators[op](a, b));// Push the computed value to the values stack
         }
         if (ops.length && ops[ops.length - 1] === "(") {
           ops.pop();
         }
-      } else if (this.isCellReference(token)) {
+      } else if (this.isCellReference(token)) { //
         const [value, error] = this.getCellValue(token);
         if (error) {
           this._errorMessage = error;
@@ -102,21 +129,43 @@ export class FormulaEvaluator {
         this._errorMessage = ErrorMessages.invalidFormula;
         return;
       }
+
     }
 
     while (ops.length) {
-      const op = ops.pop() as Operator;;
+      const op = ops.pop() as Operator;
+      if (!values.length) {
+        // Check if there are no values left to pop (i.e., not enough operands)
+        this._errorMessage = ErrorMessages.invalidFormula;
+        this._result = NaN; // or any suitable value to indicate an error
+        return;
+      }
       const b = values.pop()!;
+      if (!values.length) {
+        // If there's an operator but not enough operands
+        this._errorMessage = ErrorMessages.invalidFormula;
+        this._result = b;  // set the result to the only number present
+        return;
+      }
       const a = values.pop()!;
       values.push(operators[op](a, b));
     }
 
+
+
     this._result = values[0] || 0;
   }
+  /**
+  * Gets the error message if any error occurred during formula evaluation.
+  */
 
   public get error(): string {
     return this._errorMessage
   }
+
+  /**
+   * Gets the result of the formula evaluation.
+   */
 
   public get result(): number {
     return this._result;
